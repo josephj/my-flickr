89602e578bf9ee208cdb4b03ebb94ce0560f7f2f
josephj <josephj6802@gmail.com>
2016-12-07 09:15:11 +1100

    Save to cache

--- public/ServiceWorker.js
+++ public/ServiceWorker.js
@@ -8,9 +8,7 @@ this.addEventListener('install', function (e) {
         './fallback.html',
         './index.html',
         './ServiceWorker.js',
-        './static/js/bundle.js',
-        './static/js/main.*.js',
-        './static/css/main.*.css'
+        './static/js/bundle.js'
       ])
     })
   );
@@ -19,17 +17,21 @@ this.addEventListener('install', function (e) {
 this.addEventListener('fetch', (e) => {
   var request = e.request;
   var requestURL = new URL(e.request.url);
-  if (requestURL.hostname === 'api.flickr.com') { // API
-    console.info(requestURL.hostname);
+  if (
+    requestURL.hostname === 'api.flickr.com' ||
+    /\.staticflickr\.com$/.test(requestURL.hostname)
+  ) {
     e.respondWith(
       caches.match(e.request)
-      .then(response => response || fetch(e.request))
+      .then(response => response || fetch(e.request).then(response => {
+        return caches.open('v5').then(cache => {
+          cache.put(e.request, response.clone());
+          return response;
+        })
+      }))
     )
-  } else if (/\.staticflickr\.com$/.test(requestURL.hostname)) { // Images
-    e.respondWith(
-      caches.match(e.request)
-      .then(response => response || fetch(e.request))
-    )
+  } else if (e.request.url.indexOf('sockjs-node') !== -1)  { // Other assets
+    e.respondWith(fetch(e.request));
   } else { // Other assets
     e.respondWith(
       caches.match(e.request)
